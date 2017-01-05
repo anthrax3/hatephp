175117850e3ccb63c672775ccf3fe3273d24bf70
0v3rl0w <0v3rl0w@gmail.com>
2017-01-05 19:51:04 +0100

    Hate PHP and Love Python

---
+++ htmlparse.py
@@ -0,0 +1,23 @@
+import os
+import post
+
+def post_parse(python, client, data):
+    form = {}
+
+    for i in data:
+        o = i.split("=")
+        print(o)
+        o[1] = o[1].replace("+", " ")
+        form[o[0]] = o[1]
+
+    script = open(python, "r").read().split("\n")
+    for i in script:
+        exec(i)
+
+def parse(code, client, filename="index.html"):
+    code.remove(code[0])
+    send(code, client)
+
+def send(data, client):
+    for i in data:
+        client.send(i.encode("utf-8"))
