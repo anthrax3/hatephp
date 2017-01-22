3f83dd7086c660d1e9997c05bd2c189b078e3e42
0v3rl0w <0v3rl0w@gmail.com>
2017-01-22 14:43:27 +0100

    Version 0.5
    Bug sous Firefox fixé

--- htmlparse.py
+++ htmlparse.py
@@ -1,8 +1,8 @@
-import os
+#-*- coding : utf-8 -*-
+#!/usr/bin/env python3
 
 def post_parse(python, client, data):
     form = {}
-
     for i in data:
         o = i.split("=")
         o[1] = o[1].replace("+", " ")
@@ -11,10 +11,23 @@ def post_parse(python, client, data):
     script = open(python, "r").read().split("\n")
     for i in script:
         exec(i)
+    client.close()
 
 def parse(code, client, filename="index.html"):
-    code.remove(code[0])
     send(code, client)
+    client.close()
+
+def find_data(msg):
+    suivant = False
+    data = []
+    print(msg.encode("utf-8"))
+    for ligne in msg.split("\n"):
+        if ligne == "\r":
+            suivant = True
+        if suivant and ligne != "\r":
+            data.append(ligne)
+
+    return data
 
 def send(data, client):
     for i in data:
--- httpserver.py
+++ httpserver.py
@@ -1,27 +1,33 @@
 #-*- coding : utf-8 -*-
+#!/usr/bin/env python3
 
 import socket
 from htmlparse import *
 
 socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-socket.bind(("", 80))
 
+socket.bind(("", 80))
 socket.listen(10)
 
 while True:
 	client, info = socket.accept()
 	msg = client.recv(1024).decode("utf-8")
+	print(msg)
 	if "GET" in msg:
 		method = msg.split("\n")[0].split(" ")
+
 		if method[1] == "/":
 			file = open("index.html", "r")
 			parse(file.read().split("\n"), client)
+
 		elif method[1] == "/favicon.ico":
 			continue
+
 		else:
 			file = open(method[1][1:], "r")
 			parse(file.read().split("\n"), client, method[1][1:])
+
 	elif "POST" in msg:
 		filename = msg.split("\n")[0].split(" ")[1][1:]
-		data = msg.split("\n")[14:]
+		data = find_data(msg)
 		post_parse(filename, client, data)
